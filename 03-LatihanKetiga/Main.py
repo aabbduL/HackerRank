2b495cc576cf3d18e672ea638dc475ea6fda7b90
Abdul Karim Zaidan <zaidan.abdul22@gmail.com>
2022-06-25 21:12:46 +0700

    latihan 3

--- 03-LatihanKetiga/Main.py
+++ 03-LatihanKetiga/Main.py
@@ -1,28 +1,23 @@
 # Khusus Matrix 3x3
 
-arr = []
+def Func(arr):
+    d1, d2 = 0, 0
+    n = len(arr)
 
-x = input("Masukkan Angka Baris Pertama : ").split()
-y = input("Masukkan Angka Baris Kedua   : ").split()
-z = input("Masukkan Angka Baris Ketiga  : ").split()
+    for i in range(n):
+        d1 += arr[i][i]
+        d2 += arr[i][n-i-1]
 
-i = list(map(int, x))
-j = list(map(int, y))
-k = list(map(int, z))
+    print(f"\nMatrix arr = {arr}")
+    print(f"Output => {abs(d1-d2)}\n")
 
-arr.append(i)
-arr.append(j)
-arr.append(k)
+if __name__ == '__main__':
+    n = int(input("Masukkan jumlah baris matrix: ").strip())
 
-print(f"\nMatrix arr = {arr}")
+    arr = []
 
-d1,d2 = 0, 0
-n = len(arr)
-
-for i in range(n):
-    d1 += arr[i][i]
-    d2 += arr [i][n-i-1]
-
-print("")
-print(f"Output => {abs(d1-d2)}\n")
+    for _ in range(n):
+        arr.append(list(map(int, input("Masukkan angka: ").rstrip().split())))
+    
+    Func(arr)
 
