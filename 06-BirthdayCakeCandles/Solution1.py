ad6ef2d4082c70fe5739b5604c0fd8d12cc4ace3
Abdul Karim Zaidan <zaidan.abdul22@gmail.com>
2023-04-09 10:16:22 +0700

    another question

---
+++ 06-BirthdayCakeCandles/Solution1.py
@@ -0,0 +1,18 @@
+def birthdayCakeCandles(ar):
+    total = 0
+    highest = max(ar)
+
+    for candles in ar:
+        if candles == highest:
+            total += 1
+    
+    return total
+
+if __name__ == '__main__':
+
+    count = int(input("\nMasukkan jumlah lilin yang tersedia : "))
+    ar = list(map(int, input("Masukan tinggi dari setiap lilin yang tersedia : ").rstrip().split()))
+    
+    result = birthdayCakeCandles(ar)
+    print(f"\nTotal lilin yang tertiup ada : {result} buah lilin\n")
+
---
+++ 06-BirthdayCakeCandles/Solution2.py
@@ -0,0 +1,12 @@
+def birthdayCakeCandles(ar):
+    highest = max(ar)
+    return len([candles for candles in ar if candles == highest])
+
+if __name__ == '__main__':
+
+    count = int(input("\nMasukkan jumlah lilin yang tersedia : "))
+    ar = list(map(int, input("Masukan tinggi dari setiap lilin yang tersedia : ").rstrip().split()))
+    
+    result = birthdayCakeCandles(ar)
+    print(f"\nTotal lilin yang tertiup ada : {result} buah lilin\n")
+
